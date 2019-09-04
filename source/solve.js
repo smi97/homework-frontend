80ba15cba43b3b071a27254648c9488f19fc6e9a
smi97 <smi97@mail.ru>
2019-09-04 18:19:03 +0300

    Changed eval to new Function.
    Added checking arguments.
    
    Added some tests.

--- source/solve.js
+++ source/solve.js
@@ -1,2 +1,17 @@
 'use strict';
-const solve = (equation, x) => eval(equation.replace('x', x.toString()));
+let solve = function(equation, x)  {
+    if(/x{2,}/.test(equation) === true) { // проверяем, что нет последовательностей из более, чем одного Х (например, хх)
+        return 'error';
+    }
+    let new_eq = equation.replaceAll(/x/, x.toString()); // подставляем иксы
+
+    if(/^[\d+\-*()/ ]*$/.test(new_eq) === true) { // проверяем, что выражение содержит только цифры и знаки
+        return (new Function( 'return (' + new_eq + ')' )()); // вычисляем выражение
+    } else {
+        return 'error';
+    }
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    return this.replace(new RegExp(search, 'g'), replacement);
+};
--- tests/solve.js
+++ tests/solve.js
@@ -8,5 +8,15 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
+		assert.strictEqual(solve('x', 3), 3);
+		assert.strictEqual(solve('3', 3), 3);
+		assert.strictEqual(solve('x+1', 2), 3);
+		assert.strictEqual(solve('( x + 1 )/3', 2), 1);
+		assert.strictEqual(solve('xxx', 3), 'error');
+		assert.strictEqual(solve('xx', 3), 'error');
+		assert.strictEqual(solve('1 - a - x', 3), 'error');
+		assert.strictEqual(solve('1 - a - a', 3), 'error');
+		assert.strictEqual(solve('a', 3), 'error');
+		assert.strictEqual(solve('xa', 3), 'error');
 	});
 });
