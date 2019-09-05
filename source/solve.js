793bc4dfdbad5bc7e65a9b3408fcb7dc0c01f0dd
smi97 <smi97@mail.ru>
2019-09-05 13:21:20 +0300

    Separated if conditions

--- source/solve.js
+++ source/solve.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const solve = (equation, x) => {
-    if ((/x{2,}/.test(equation)) // нет последовательностей из более, чем одного Х (например, хх)
-        || (typeof(equation) !== "string" && typeof(equation) !== "number") // выражение либо строка, либо число
-        || (typeof(x) !== "number" && typeof(x) !== "string") ) { // икс либо строка, либо число
-        return 'error';
+    if ((/x{2,}/.test(equation))) // нет последовательностей из более, чем одного Х (например, хх)
+    {
+        return 'error: variable wrong name';
     }
+
+    if (typeof(equation) !== "string") // выражение либо строка, либо число
+    {
+        return 'error: equation wrong type';
+    }
+
+    if ((typeof(x) !== "number" && typeof(x) !== "string") ) { // икс либо строка, либо число
+        return 'error: x wrong type';
+    }
+
     if (/^[x\d+\-*()/ ]+$/.test(equation) // выражение содержит только цифры и знаки
         && /\d+/.test(x)) { // икс содержит только цифры
         return new Function(`x`, `return parseInt(${equation})`)(x); // вычисляем выражение
--- tests/solve.js
+++ tests/solve.js
@@ -2,10 +2,6 @@
 
 QUnit.module('Тестируем функцию solve', function () {
 	QUnit.test('solve работает правильно ', function (assert) {
-		let obj = {equation: "test",
-			x: 30 };
-		let nullTest = null;
-		assert.strictEqual(solve('x + 1', 1), 2);
 		assert.strictEqual(solve('2 + x - 1', 5), 6);
 		assert.strictEqual(solve('2 * x - 1', 5), 9);
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
@@ -15,17 +11,18 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('3', 3), 3);
 		assert.strictEqual(solve('x+1', 2), 3);
 		assert.strictEqual(solve('( x + 1 )/3', 2), 1);
-		assert.strictEqual(solve('xxx', 3), 'error');
-		assert.strictEqual(solve('xx', 3), 'error');
+		assert.strictEqual(solve('xxx', 3), 'error: variable wrong name');
+		assert.strictEqual(solve('xx', 3), 'error: variable wrong name');
 		assert.strictEqual(solve('1 - a - x', 3), 'error');
 		assert.strictEqual(solve('1 - a - a', 3), 'error');
 		assert.strictEqual(solve('a', 3), 'error');
 		assert.strictEqual(solve('xa', 3), 'error');
-		assert.strictEqual(solve(2322, 3), 2322);
+		assert.strictEqual(solve(2322, 3), 'error: equation wrong type');
 		assert.strictEqual(solve('x', '2'), 2);
-		assert.strictEqual(solve(obj, 3), 'error');
-		assert.strictEqual(solve(nullTest, 3), 'error');
+		assert.strictEqual(solve({equation: "test", x: 30 }, 3), 'error: equation wrong type');
+		assert.strictEqual(solve(null, 3), 'error: equation wrong type');
 		assert.strictEqual(solve('x', 'x'), 'error');
+		assert.strictEqual(solve('x', null), 'error: x wrong type');
 
 	});
 });
